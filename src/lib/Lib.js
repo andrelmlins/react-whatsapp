2c5316137e9698690db38bf82bdf031dcdcfb1f5
André Lins <andrelucas01@hotmail.com>
2020-04-02 16:32:33 -0300

    "First Commit"

---
+++ src/lib/Lib.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Lib = props => (
+  <button
+    style={{
+      border: 'none',
+      boxShadow: 'none',
+      backgroundColor: '#00838f',
+      color: '#FFF',
+      padding: '0px 20px',
+      fontSize: 20,
+      height: 40
+    }}
+    {...props}
+  />
+);
+
+export default Lib;
