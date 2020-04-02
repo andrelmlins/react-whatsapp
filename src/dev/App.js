5975719487c0b0bc6d524a4ee4e4bc4b885902c2
André Lins <andrelucas01@hotmail.com>
2020-04-02 16:57:12 -0300

    Create component

--- src/dev/App.js
+++ src/dev/App.js
@@ -1,13 +1,27 @@
-import React from 'react';
-import Button from '../lib';
-import logo from './logo.png';
+import React from "react";
+import ReactWhatsapp from "../lib";
+import logo from "./logo.png";
 
 const App = () => (
   <div className="content">
     <img src={logo} className="logo" alt="Logo Create React Dependency" />
     <h1 className="title">Create React Dependency</h1>
     <section className="library">
-      <Button onClick={() => alert('button')}>My Library Button</Button>
+      <ReactWhatsapp
+        number="+055 (81) 98852-7655"
+        message="Meu envio"
+        style={{
+          border: "none",
+          boxShadow: "none",
+          backgroundColor: "#00838f",
+          color: "#FFF",
+          padding: "0px 20px",
+          fontSize: 20,
+          height: 40
+        }}
+      >
+        Abrir Whatsapp
+      </ReactWhatsapp>
     </section>
   </div>
 );
--- src/lib/Lib.js
+++ src/lib/Lib.js
@@ -1,18 +1,41 @@
-import React from 'react';
+import React from "react";
+import PropTypes from "prop-types";
 
-const Lib = props => (
-  <button
-    style={{
-      border: 'none',
-      boxShadow: 'none',
-      backgroundColor: '#00838f',
-      color: '#FFF',
-      padding: '0px 20px',
-      fontSize: 20,
-      height: 40
-    }}
-    {...props}
-  />
-);
+const URL = " https://wa.me";
 
-export default Lib;
+const ReactWhatsapp = ({ number, message, element, onClick, ...props }) => {
+  const Element = element;
+
+  number = number.replace(/[^\w\s]/gi, "").replace(/ /g, "");
+
+  let url = `${URL}/${number}`;
+
+  if (message) {
+    url += `?text=${encodeURI(message)}`;
+  }
+
+  return (
+    <Element
+      onClick={e => {
+        window.open(url);
+
+        if (onClick) {
+          onClick(e);
+        }
+      }}
+      {...props}
+    />
+  );
+};
+
+ReactWhatsapp.propTypes = {
+  number: PropTypes.string.isRequired,
+  message: PropTypes.string,
+  element: PropTypes.element
+};
+
+ReactWhatsapp.defaultProps = {
+  element: "button"
+};
+
+export default ReactWhatsapp;
