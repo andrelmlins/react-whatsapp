04a06f934a4308de5a0a2f4675c023d6a8cba940
André Lins <andrelucas01@hotmail.com>
2020-09-07 11:27:22 -0300

    More tests

--- src/lib/ReactWhatsapp.spec.js
+++ src/lib/ReactWhatsapp.spec.js
@@ -1,5 +1,10 @@
 import React from 'react';
-import { render, cleanup } from '@testing-library/react';
+import {
+  render,
+  cleanup,
+  getByLabelText,
+  fireEvent
+} from '@testing-library/react';
 
 import ReactWhatsapp from './index';
 
@@ -7,8 +12,45 @@ afterEach(cleanup);
 
 describe('ReactWhatsapp Component', () => {
   it('Should render the component', () => {
+    const { container } = createComponent({
+      number: '+1-202-555-0107',
+      message: 'MESSAGE'
+    });
+    expect(container).toBeDefined();
+  });
+
+  it('Should render the component without message', () => {
     const { container } = createComponent({ number: '+1-202-555-0107' });
     expect(container).toBeDefined();
+  });
+
+  it('Call button with onClick', () => {
+    const onClick = jest.fn();
+    window.open = jest.fn();
+
+    const { getByLabelText } = createComponent({
+      number: '+1-202-555-0107',
+      onClick,
+      'aria-label': 'Click'
+    });
+
+    fireEvent.click(getByLabelText('Click'));
+
+    expect(onClick).toHaveBeenCalledTimes(1);
+    expect(window.open).toHaveBeenCalledTimes(1);
+  });
+
+  it('Call button without onClick', () => {
+    window.open = jest.fn();
+
+    const { getByLabelText } = createComponent({
+      number: '+1-202-555-0107',
+      'aria-label': 'Click'
+    });
+
+    fireEvent.click(getByLabelText('Click'));
+
+    expect(window.open).toHaveBeenCalledTimes(1);
   });
 });
 
